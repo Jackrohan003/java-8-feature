d79000432fb68a2b1cc5d6b2c85d741d6745fee9
Rohan Prajapati <58733914+Jackrohan003@users.noreply.github.com>
2021-02-04 15:02:09 +0530

    Stream api with Examples:

--- StreamExamples.java
+++ StreamExamples.java
@@ -2,10 +2,76 @@ import java.util.*;
 class Main {  
   public static void main(String args[]) { 
     
-    List<Integer> list = Arrays.asList(2,3,4,5);
-    
-    System.out.println(list.stream().map(num->num*2).reduce(0,(c,e)->c+e));//It will give result 28
+    List<String> stringCollection = new ArrayList<>();
+stringCollection.add("ddd2");
+stringCollection.add("aaa2");
+stringCollection.add("bbb1");
+stringCollection.add("aaa1");
+stringCollection.add("bbb3");
+stringCollection.add("ccc");
+stringCollection.add("bbb2");
+stringCollection.add("ddd1");
+
+//Filter
+stringCollection
+    .stream()
+    .filter((s) -> s.startsWith("a"))
+    .forEach(System.out::println);
+
+//Sorted is an intermediate operation which returns a sorted view of the stream. The elements are sorted in natural order unless you pass a custom Comparator.
+
+stringCollection
+    .stream()
+    .sorted()
+    .forEach(System.out::println);
+
+//Map :The intermediate operation map converts each element into another object via the given function. The following example converts each string into an upper-cased string. But you can also use map to transform each object into another type. The generic type of the resulting stream depends on the generic type of the function you pass to map
+
+stringCollection
+    .stream()
+    .map(String::toUpperCase)
+    .sorted((a, b) -> b.compareTo(a))
+    .forEach(System.out::println);
+
+    //Match :Various matching operations can be used to check whether a certain predicate matches the stream. All of those operations are terminal and return a boolean result.
+    boolean anyStartsWithA =
+    stringCollection
+        .stream()
+        .anyMatch((s) -> s.startsWith("a"));
+
+System.out.println(anyStartsWithA);      // true
+
+boolean allStartsWithA =
+    stringCollection
+        .stream()
+        .allMatch((s) -> s.startsWith("a"));
+
+System.out.println(allStartsWithA);      // false
+
+boolean noneStartsWithZ =
+    stringCollection
+        .stream()
+        .noneMatch((s) -> s.startsWith("z"));
+
+System.out.println(noneStartsWithZ);      // true
+
+//Count : Count is a terminal operation returning the number of elements in the stream as a long.
+long startsWithB =
+    stringCollection
+        .stream()
+        .filter((s) -> s.startsWith("b"))
+        .count();
+
+System.out.println(startsWithB);    // 3
+//Reduce : This terminal operation performs a reduction on the elements of the stream with the given function. The result is an Optional holding the reduced value.
+
+Optional<String> reduced =
+    stringCollection
+        .stream()
+        .sorted()
+        .reduce((s1, s2) -> s1 + "#" + s2);
+
+reduced.ifPresent(System.out::println);
     
   } 
-  
 }
