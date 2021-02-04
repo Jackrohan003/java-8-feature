149ec277c83e7eca0889b501feb5288dae32963d
Rohan Prajapati <58733914+Jackrohan003@users.noreply.github.com>
2021-02-04 12:37:16 +0530

    ForEach Method in java 8

---
+++ ForEachMethod.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class ForEachMethod {  
+  public static void main(String args[]) { 
+    System.out.println("Hello, world!"); 
+    List<Integer> list = Arrays.asList(2,3,4,5);
+    list.forEach(num->System.out.println(num));
+    
+    List<String> stringList = Arrays.asList("Rohan","Varun","Smit");
+    
+    stringList.forEach(str->System.out.println(str));
+    
+  } 
+}
